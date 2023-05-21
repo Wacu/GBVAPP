783adeeeb24e435b3fec66436c66930321e0eb15
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2023-05-22 00:38:54 +0300

    First commit

---
+++ Pages/02.Page2.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from apps.db import *
+
+tablename='scrape_tweets'
+
+option = st.selectbox('Select data ',['Select one of the below','Choose data from DB', 'Upload Data'],index=0)
+
+if option == 'Select one of the below':
+    pass
+
+if option=='Choose data from DB':
+
+    with st.form("my_form"):
+        # select_table    =   st.selectbox('Select Table',[read_number_tables])
+        number_of_rows  =   st.number_input('Number of Rows')
+
+        submitted =  st.form_submit_button("Submit")
+        if submitted:
+            
+            data=read_data(int(number_of_rows),tablename)
+            st.success(f'The data of {data.shape[0]} has been successful loaded')
+            st.table(data.head(2))
+
+expander_clean=st.expander('This section View the Selected Data and Go through the Cleaning Process👇')
+options_clean=st.selectbox('Select Text  Cleaning Steps',['Text Cleaner'],label_visibility="visible" )
+
+
+            
+
+    
+
+
+
---
+++ apps/db.py
@@ -0,0 +1,55 @@
+import sqlite3
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+query = "SELECT * FROM users"
+tablename='scrape_tweets'
+
+def create_table(df,tablename):
+
+    try:
+        conn = sqlite3.connect('tweets.db')
+        df.to_sql(tablename, conn, if_exists='replace', index=False)
+        conn.close()  
+    except Exception as error:
+        print(error)
+
+
+def read_data(numrows,tablename):
+
+    try:
+        query = f"SELECT  * FROM {tablename} LIMIT {numrows}"
+        conn = sqlite3.connect('tweets.db')
+        df = pd.read_sql_query(query, conn)
+        conn.close() 
+        return df 
+    except Exception as error:
+        print(error)
+
+def to_format_our_data_before_store(df):
+
+
+    df['date_'] = df['date'].dt.strftime('%d/%m/%Y')
+    df['date_']= pd.to_datetime(df['date_'])
+    df.drop(columns=['date'],inplace=True)
+
+    return df
+
+
+def read_number_tables():
+    conn = sqlite3.connect('tweets.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    list_of_tables =[table[0] for table in tables]
+    conn.close()
+    return list_of_tables 
+
+print(read_number_tables)
+
+
+
+
+
+
