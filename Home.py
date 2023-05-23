60c04101f356d5009538665903572467bf026176
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2023-05-23 23:20:13 +0300

    updated

--- Home.py
+++ Home.py
@@ -19,4 +19,4 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title(" Natural Language Processing")
 
-st.image('https://github.com/Wacu/GBVAPP/blob/5da999594ed361ba5f83510d2919f858f9e4729f/GBV.png',use_column_width=True)
+st.image('https://github.com/Wacu/GBVAPP/blob/5da999594ed361ba5f83510d2919f858f9e4729f/GBV.png',use_column_width=False)
